d60afb27d138e987ff74fc80734ff8002f749b48
Anurag1096 <anuragqwerty09@outlook.com>
2025-08-27 21:14:01 +0530

    wip: starting component design

---
+++ src/component/Timer/index.tsx
@@ -0,0 +1,13 @@
+import { TimerState } from "./types";
+const Timer =()=>{
+    //The timer will have three properties 
+    // hour, min and second.
+    // The timer component could be manipulated from out side 
+    // 
+    
+    return (<>
+    
+    </>)
+}
+
+export default Timer;
